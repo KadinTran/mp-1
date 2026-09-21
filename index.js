75737947bdb2364107febe9b870efc3699f3e506
Kadin Tran <kadinthientran@gmail.com>
2026-09-21 17:15:14 -0400

    fixed nav small screen overflow and added reactions to calculator

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ function  addi() {
     let out = Number(f) + Number(s);
     // ternary conditional operator is smtn i rmbd from java
     // I googled if it works in JS too: https://www.w3schools.com/JS/js_if_ternary.asp
-    out < 0 ? document.getElementById("output").classList.add('negative')
-        : document.getElementById("output").classList.remove('negative');
-    document.getElementById("output").innerHTML = out;
+    display(out);
     console.log("add");
 }
 
@@ -15,9 +13,7 @@ function  subt() {
     let f = document.getElementById('first_number').value;
     let s = document.getElementById('second_number').value;
     let out = Number(f) - Number(s);
-    out < 0 ? document.getElementById("output").classList.add('negative')
-        : document.getElementById("output").classList.remove('negative');
-    document.getElementById("output").innerHTML = out;
+    display(out);
     console.log("sub");
 }
 
@@ -25,9 +21,7 @@ function  mult() {
     let f = document.getElementById('first_number').value;
     let s = document.getElementById('second_number').value;
     let out = Number(f) * Number(s);
-    out < 0 ? document.getElementById("output").classList.add('negative')
-        : document.getElementById("output").classList.remove('negative');
-    document.getElementById("output").innerHTML = out;
+    display(out);
     console.log("mul");
 }
 
@@ -35,9 +29,7 @@ function  divi() {
     let f = document.getElementById('first_number').value;
     let s = document.getElementById('second_number').value;
     let out = Number(f) / Number(s);
-    out < 0 ? document.getElementById("output").classList.add('negative')
-        : document.getElementById("output").classList.remove('negative');
-    document.getElementById("output").innerHTML = out;
+    display(out);
     console.log("div");
 }
 
@@ -50,14 +42,40 @@ function  expo() {
         out *= f;
     }
     let res = s < 0 ? 1 / out : out;
-    out < 0 ? document.getElementById("output").classList.add('negative')
-        : document.getElementById("output").classList.remove('negative');
-    document.getElementById("output").innerHTML = res;
+    display(res);
     console.log("exponents");
 }
 
+function  display(out) {
+    if (out > 1000){
+        document.getElementById("judgement").setAttribute("src", "../imgs/cheer.jpeg");
+        document.getElementById("judgement").setAttribute("alt", "a blue cheering emoji");
+    }
+    else if (out === 67){
+        document.getElementById("judgement").setAttribute("src", "../imgs/beast.jpeg");
+        document.getElementById("judgement").setAttribute("alt", "a blue angry beast emoji");
+    }
+    else if (out === 0){
+        document.getElementById("judgement").setAttribute("src", "../imgs/nervous.jpeg");
+        document.getElementById("judgement").setAttribute("alt", "a blue nervous emoji");
+    }
+    else if (out < 0){
+        document.getElementById("output").classList.add('negative')
+        document.getElementById("judgement").setAttribute("src", "../imgs/sad.jpeg");
+        document.getElementById("judgement").setAttribute("alt", "a blue sad emoji");
+
+    } else {
+        document.getElementById("output").classList.remove('negative');
+        document.getElementById("judgement").setAttribute("src", "../imgs/happy.jpeg");
+        document.getElementById("judgement").setAttribute("alt", "a blue emoji with thumbs up");
+    }
+    document.getElementById("output").innerHTML = "Result: " + out;
+}
+
 function  clears() {
-    document.getElementById("output").innerHTML = "";
+    document.getElementById("output").innerHTML = "Result:";
     document.getElementById("first_number").value = "";
     document.getElementById("second_number").value = "";
+    document.getElementById("judgement").setAttribute("src", "../imgs/neutral.jpeg");
+    document.getElementById("judgement").setAttribute("alt", "a blue emoji with a neutral smile");
 }
